039a5640a1d671e8eb5bea1a031b20c4327c5e3c
zyc <art_kernel_zyc@163.com>
2015-06-21 19:12:31 +0800

    cell    ----   dingzhi

---
+++ XHHealthProject/FirstCell.h
@@ -0,0 +1,13 @@
+//
+//  FirstCell.h
+//  XHHealthProject
+//
+//  Created by 菁优数学 on 15/6/21.
+//  Copyright (c) 2015年 AlexYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstCell : UITableViewCell
+
+@end
---
+++ XHHealthProject/FirstCell.m
@@ -0,0 +1,23 @@
+//
+//  FirstCell.m
+//  XHHealthProject
+//
+//  Created by 菁优数学 on 15/6/21.
+//  Copyright (c) 2015年 AlexYang. All rights reserved.
+//
+
+#import "FirstCell.h"
+
+@implementation FirstCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
