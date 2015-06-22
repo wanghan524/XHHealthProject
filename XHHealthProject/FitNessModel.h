e2911974c805670ec9da2651621d0a2bd8260c3d
zyc <art_kernel_zyc@163.com>
2015-06-22 16:59:39 +0800

    fitness model

---
+++ XHHealthProject/FitNessModel.h
@@ -0,0 +1,27 @@
+//
+//  FitNessModel.h
+//  XHHealthProject
+//
+//  Created by 菁优数学 on 15/6/22.
+//  Copyright (c) 2015年 AlexYang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FitNessModel : NSObject
+
+@property(nonatomic,copy)NSString *Jump_cn;
+@property(nonatomic,copy)NSString *Jump_value;
+@property(nonatomic,copy)NSString *Jump_refinterVal;
+
+
+@property(nonatomic,copy)NSString *Grip_cn;
+@property(nonatomic,copy)NSString *Grip_value;
+@property(nonatomic,copy)NSString *Grip_refinterVal;
+
+@property(nonatomic,copy)NSString *Bending_cn;
+@property(nonatomic,copy)NSString *Bending_value;
+@property(nonatomic,copy)NSString *Bending_refinterVal;
+@property(nonatomic,copy)NSString *Exmessage;
+
+@end
---
+++ XHHealthProject/FitNessModel.m
@@ -0,0 +1,13 @@
+//
+//  FitNessModel.m
+//  XHHealthProject
+//
+//  Created by 菁优数学 on 15/6/22.
+//  Copyright (c) 2015年 AlexYang. All rights reserved.
+//
+
+#import "FitNessModel.h"
+
+@implementation FitNessModel
+
+@end
